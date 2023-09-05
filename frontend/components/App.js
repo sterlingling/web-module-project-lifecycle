7b2b6850ca5ea829c8bcada2f468a8080b768f3b
Sterling Sanders <sterlingsanders723@gmail.com>
2023-09-04 22:49:58 -0700

    yippee

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,75 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
-export default class App extends React.Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      error: '',
+      todoNameInput: '',
+    }
+
+
+  }
+
+  resetForm = () => this.setState({ ...this.state, todoNameInput: '' });
+
+  setAxiosResponseError = (err) => this.setState({ ...this.state, error: err.response.data.message })
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
+  postNewTodo = () => {
+    axios.post(URL, { name: this.state.todoNameInput })
+      .then(res => {
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
+        this.resetForm();
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
+  componentDidMount() {
+    this.fetchAllTodos();
+  }
+
+  inputChange = evt => {
+    const { value } = evt.target
+    this.setState({ ...this.state, todoNameInput: value })
+  }
+
+  onTodoFormSubmit = evt => {
+    evt.preventDefault();
+    this.postNewTodo();
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>Error: {this.state.error}</div>
+        <div id='todos'>
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
+        </div>
+        <form id='todoForm' onSubmit={this.onTodoFormSubmit}>
+          <input onChange={this.inputChange} value={this.state.todoNameInput} type='text' placeholder='Type todo'></input>
+          <input type='submit'></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
+
+export default App;
--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './components/App'
 import './styles/reset.css'
 import './styles/styles.css'
 
-render(
-  <React.StrictMode>
-    <h1>Todo App AJAX</h1>
-    <App />
-  </React.StrictMode>
-  , document.getElementById('root')
-)
+
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
+
