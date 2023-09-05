cd174096f804659fc1205abe350e0c134372eb95
Sterling Sanders <sterlingsanders723@gmail.com>
2023-09-04 23:07:24 -0700

    progress

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -45,6 +45,19 @@ class App extends React.Component {
     this.setState({ ...this.state, todoNameInput: value })
   }
 
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        this.setState({
+          ...this.state, todos: this.state.todos.map(td => {
+            if (td.id != id) return td
+            return res.data.data
+          })
+        })
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
   onTodoFormSubmit = evt => {
     evt.preventDefault();
     this.postNewTodo();
@@ -58,7 +71,7 @@ class App extends React.Component {
           <h2>Todos:</h2>
           {
             this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
+              return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ☑️' : ''}</div>
             })
           }
         </div>
